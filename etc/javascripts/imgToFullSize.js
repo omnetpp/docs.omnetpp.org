3195b7148cf023226393dae0b64163bf551fa633
TÖRÖK Attila <torokati44@gmail.com>
2017-10-03 15:17:56 +0200

    support thumbnail images, styling from css, better layout

--- etc/javascripts/imgToFullSize.js
+++ etc/javascripts/imgToFullSize.js
@@ -1,6 +1,10 @@
 imageFullSizeZoom = function(img){
-img = $(img);
+    img = $(img);
+    let fullSizeSrc = img.attr('src').replace(/.thumb.jpg$/, '.png');
 
-$(document.body).append('<div style="cursor:zoom-out;position:fixed;left:0;right:0;top:0;bottom:0;z-index:6969;background:rgba(0,0,0,.5);text-align:center" onclick="$(this).remove();"><img src="' + img.attr('src') + '" style="max-width:100%;max-height:100%" />');
-
+    $(document.body).append(
+      '<div class="fullscreenimgshade" onclick="$(this).remove();">' +
+        '<img src="' + fullSizeSrc + '" class="fullscreenimg"/>' +
+      '</div>'
+    );
 };
